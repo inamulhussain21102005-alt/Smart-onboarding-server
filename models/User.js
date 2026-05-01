b0a44abec7da6ed547a8c92c952f6294d3bef656
inamulhussain21102005-alt <inamulhussain21102005@gmail.com>
2026-05-01 15:50:24 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -13,22 +12,5 @@ const userSchema = new mongoose.Schema({
   status: { type: String, enum: ['active','onboarding','inactive'], default: 'onboarding' },
   onboardingProgress: { type: Number, default: 0 }
 }, { timestamps: true });
-
-userSchema.pre('save', async function(next) {
-  try {
-    if (!this.isModified('password')) {
-      return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    return next();
-  } catch(err) {
-    return next(err);
-  }
-});
-
-userSchema.methods.matchPassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 module.exports = mongoose.model('User', userSchema);
